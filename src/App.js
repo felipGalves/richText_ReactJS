cdd91049421b617818e0948659d4446224e46daa
felipealves <felipealves@elevor.com>
2022-01-21 12:19:45 -0300

    adding heading and paragraph

--- src/App.js
+++ src/App.js
@@ -10,63 +10,36 @@ import {
   faAlignLeft,
   faAlignCenter,
   faAlignRight,
-  faAlignJustify,
-  faImage,
-  faHeading
+  faHeading,
+  faParagraph
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-// Select 
-import Select from 'react-select';
-
-// Opções select
-const options = [
-  { value: 'Paragraph', label: 'Paragraph' },
-  { value: 'Heading3', label: 'Heading 3' },
-  { value: 'Heading2', label: 'Heading 2' },
-  { value: 'Heading1', label: 'Heading 1' }
-]
-
 function App() {
- function onSelectChange({ value }) {
-    console.log(value)
-
-    switch (value) {
-      case 'Paragraph':
-        document.execCommand('formatBlock', false, 'p'); 
-          break;
-      case 'Heading3':
-        document.execCommand('formatBlock', false, '<h3>'); 
-          break;
-      case 'Heading2':
-          document.execCommand('formatBlock', false, '<h2>'); 
-        break;
-      case 'Heading1':
-          document.execCommand('formatBlock', false, '<h1>'); 
-        break;
-      default:
-          document.execCommand('formatBlock', false, 'p'); 
-        break;
-    }
-  };
+  function handleAddLink() {
+    const linkURL = prompt('Enter a URL:', 'http://');
+    const newLink = document.execCommand('createlink', false, linkURL);
+    newLink.target = "_blank";
+  }
 
   return (
     <div className='container'>
-        <Button />
         <div className="content">
           {/* Toolbar */}
           <section className='toolbar'>
-            <Select  
-              onChange={onSelectChange}
-              defaultValue={options[0]}
-              isDisabled={false}
-              isLoading={false}
-              isClearable={false}
-              isRtl={false}
-              isSearchable={true}
-              name="Heading"
-              options={options} 
-            />
+            <button 
+              onClick={e => document.execCommand('formatBlock', false, 'p')}
+            >
+              <FontAwesomeIcon icon={faParagraph} />
+            </button>
+
+            <button 
+              onClick={e => document.execCommand('formatBlock', false, 'h1')}
+            >
+              <FontAwesomeIcon icon={faHeading} />
+            </button>
+
+            <span className='divider'></span>
 
             <button 
               onClick={e => document.execCommand("bold", false, null)}
@@ -86,23 +59,7 @@ function App() {
               <FontAwesomeIcon icon={faUnderline} />
             </button>
 
-            <button 
-              onClick={e => document.execCommand("insertUnorderedList", false, null)}
-            >
-              <FontAwesomeIcon icon={faListUl} />
-            </button>
-
-            <button 
-              onClick={e => document.execCommand("insertOrderedList", false, null)}
-            >
-              <FontAwesomeIcon icon={faListOl} />
-            </button>
-
-            <button 
-              onClick={e => document.execCommand("createLink", false, null)}
-            >
-              <FontAwesomeIcon icon={faLink} />
-            </button>
+            <span className='divider'></span>
 
             <button 
               onClick={e => document.execCommand("justifyLeft", false, null)}
@@ -122,10 +79,24 @@ function App() {
               <FontAwesomeIcon icon={faAlignRight} />
             </button>
 
+            <span className='divider'></span>
+
             <button 
-              onClick={e => document.execCommand("justifyFull", false, null)}
+              onClick={e => document.execCommand("insertUnorderedList", false, null)}
             >
-              <FontAwesomeIcon icon={faAlignJustify} />
+              <FontAwesomeIcon icon={faListUl} />
+            </button>
+
+            <button 
+              onClick={e => document.execCommand("insertOrderedList", false, null)}
+            >
+              <FontAwesomeIcon icon={faListOl} />
+            </button>
+
+            <button 
+              onClick={handleAddLink}
+            >
+              <FontAwesomeIcon icon={faLink} />
             </button>
           </section>
 
